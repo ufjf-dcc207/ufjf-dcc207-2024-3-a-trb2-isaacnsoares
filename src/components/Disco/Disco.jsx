74182bd7d696e71bf768eb4235e6ce64fcc89f96
Isaac Soares <isaac.soares@estudante.ufjf.br>
2025-02-06 22:13:02 -0300

    cria componente disco

---
+++ src/components/Disco/Disco.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Disco.css';
+
+const Disco = ({ tamanho }) => {
+  return (
+    <div className={`disco disco-${tamanho}`}>
+      {tamanho}
+    </div>
+  );
+};
+
+export default Disco;
