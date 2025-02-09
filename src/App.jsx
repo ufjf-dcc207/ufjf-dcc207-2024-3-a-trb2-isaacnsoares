a30d047b548e874b47855d18b75eb5efda84877a
Isaac Soares <isaac.soares@estudante.ufjf.br>
2025-02-09 15:33:15 -0300

    atualiza o app para fazer a troca de discos usando estado

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,42 @@ function App() {
     []
   ]);
 
+  const moverDisco = (origemIndex, destinoIndex) => (discoTamanho) => {
+    setTorres((torres) => {
+      //cria uma nova matriz, fazendo as mudanças necessárias
+      const novasTorres = torres.map((torre, index) => {
+        // Se for a torre de origem, cria outra sem o devido disco
+        if (index == origemIndex) {
+          return torre.filter((tamanho) => tamanho !== parseInt(discoTamanho));
+        }
+        // Se for a torre de destino, adiciona o devido disco
+        if (index == destinoIndex) {
+          return [parseInt(discoTamanho), ...torre];
+        }
+        return torre;
+      });
+      return novasTorres;
+    })
+  }
+
+  const soltar = (origemIndex) => (discoTamanho) => {
+    setTorres((torres) => {
+      // Encontra a torre de destino
+      const destinoIndex = torres.findIndex(
+        (torre) => torre.length == 0 || torre[0] > parseInt(discoTamanho)
+      );
+      // Garante que a torre de destino existe e não é a mesma de origem
+      if (destinoIndex != -1 && destinoIndex != origemIndex) {
+        moverDisco(origemIndex, destinoIndex)(discoTamanho);
+      }
+    });
+  };
+
   return (
     <>
       <div className="app">
         {torres.map((discos, index) => (
-          <Torre key={index} discos={discos}/>
+          <Torre key={index} discos={discos} soltarDisco={soltar(index)}/>
         ))}
       </div>
     </>
