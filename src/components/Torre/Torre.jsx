82934574bfe9950311e140345d0f91b07a84f4c6
Isaac Soares <isaac.soares@estudante.ufjf.br>
2025-02-09 15:09:08 -0300

    faz com que a Torre receba o disco

--- src/components/Torre/Torre.jsx
+++ src/components/Torre/Torre.jsx
@@ -2,10 +2,24 @@ import React from 'react';
 import Disco from "../Disco/Disco.jsx"
 import "./Torre.css";
 
-const Torre = ({discos}) => {
+const Torre = ({discos, soltarDisco}) => {
+
+  const soltar = (e) => {
+    // Impede o comportamento padrão do navegador
+    e.preventDefault();
+    // Pega o tamanho do disco arrastado
+    const discoTamanho = e.dataTransfer.getData('tamanho');
+    // Solta o disco usando uma função passada pelo App
+    soltarDisco(discoTamanho);
+  }
+
+  const arrastarSobre = (e) => {
+    // Torna o elemento válido para receber coisas arrastadas
+    e.preventDefault();
+  }
 
   return (
-    <div className='torre'>
+    <div className='torre' soltarDisco={soltar} arrastandoSobre={arrastarSobre}>
       <div className="torre-base"></div>
       {discos.map((tamanho) => (
         <Disco key={tamanho} tamanho={tamanho} />
