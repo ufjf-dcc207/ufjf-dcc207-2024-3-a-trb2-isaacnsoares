e218f56278604cfe3b563cdf7f468f0e40dfeecb
Isaac Soares <isaac.soares@estudante.ufjf.br>
2025-02-09 14:42:06 -0300

    faz com que o disco seja arrastável
    o disco carrega seu tamanho

--- src/components/Disco/Disco.jsx
+++ src/components/Disco/Disco.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./Disco.css";
 
 const Disco = ({ tamanho }) => {
-  return <div className={`disco disco-${tamanho}`}>{tamanho}</div>;
+
+  const levantarEArrastar = (e) => {
+    // Faz com que o disco carregue seu tamanho
+    e.dataTransfer.setData('tamanho', tamanho);
+  };
+
+  return (
+  <div className={`disco disco-${tamanho}`} draggable levantarEarrastar={levantarEArrastar}>
+    {tamanho}
+  </div>
+  );
 };
 
 export default Disco;
